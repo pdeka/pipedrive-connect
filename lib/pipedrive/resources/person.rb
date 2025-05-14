78b22b9c5059c860c935c456c0bc14626ebad5c2
mruckert <mart.ruckert@gmail.com>
2025-05-15 09:41:08 +1000

    add exact match to search with person

--- lib/pipedrive/resources/person.rb
+++ lib/pipedrive/resources/person.rb
@@ -9,8 +9,9 @@ module Pipedrive
     has_many :activities, class_name: "Activity"
 
     # GET /api/v2/persons/search
-    def self.search(term)
+    def self.search(term, exact_match)
       params = { term: term }
+      params = params.merge({ exact_match: 1 }) if exact_match
       response = request(
         :get,
         "#{resource_url}/search",
