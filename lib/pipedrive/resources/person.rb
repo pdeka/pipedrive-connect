21fc51740f743062e67a8b2c18566331188ca8a2
mruckert <mart.ruckert@gmail.com>
2025-05-15 09:39:25 +1000

    add search to person

--- lib/pipedrive/resources/person.rb
+++ lib/pipedrive/resources/person.rb
@@ -7,5 +7,21 @@ module Pipedrive
 
     has_many :deals, class_name: "Deal"
     has_many :activities, class_name: "Activity"
+
+    # GET /api/v2/persons/search
+    def self.search(term)
+      params = { term: term }
+      response = request(
+        :get,
+        "#{resource_url}/search",
+        params
+      )
+      items = response[:data]
+
+      return [] if items.nil?
+
+      items.map { |d| new(d) }
+    end
+
   end
 end
