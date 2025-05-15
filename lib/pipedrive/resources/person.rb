c797ed0ee230e5f7579f6d4be66820592db9b0ea
mruckert <mart.ruckert@gmail.com>
2025-05-15 11:16:10 +1000

    search already existed

--- lib/pipedrive/resources/person.rb
+++ lib/pipedrive/resources/person.rb
@@ -8,21 +8,5 @@ module Pipedrive
     has_many :deals, class_name: "Deal"
     has_many :activities, class_name: "Activity"
 
-    # GET /api/v2/persons/search
-    def self.search(term, exact_match)
-      params = { term: term }
-      params = params.merge({ exact_match: 1 }) if exact_match
-      response = request(
-        :get,
-        "#{resource_url}/search",
-        params
-      )
-      items = response[:data]
-
-      return [] if items.nil?
-
-      items.map { |d| new(d) }
-    end
-
   end
 end
